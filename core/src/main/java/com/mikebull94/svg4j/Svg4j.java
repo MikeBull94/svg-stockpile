307e003d6d578bfdb3bf5c2c36a6aab70d72eae6
Michael Bull <mikebull94@gmail.com>
2016-03-03 21:45:08 +0000

    Optimized imports

--- core/src/main/java/com/mikebull94/svg4j/Svg4j.java
+++ core/src/main/java/com/mikebull94/svg4j/Svg4j.java
@@ -23,7 +23,6 @@ import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.function.Predicate;
 
 /**
